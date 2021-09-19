7bc839e4f384a74de833437676d32e0128cf86a2
martvanrijthoven <mart.vanrijthoven@gmail.com>
2021-09-19 12:03:40 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='concurrentbuffer',
-    version='0.0.2',
+    version='0.0.3',
     author='Mart van Rijthoven',
     author_email='mart.vanrijthoven@gmail.com',
     packages=['concurrentbuffer'],
